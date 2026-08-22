e14b559d5f993d1542ab9a2d9d637e174db7cb1c
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-08-22 12:36:04 +0530

    day 2

--- day2/Multithreading.java
+++ day2/Multithreading.java
@@ -1,7 +1,20 @@
-class Hi {
+class Hi extends Thread {
     public void run() {
         for(int i = 0; i < 10; i++) {
             System.out.println("Hi");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println("Something went wrong");
+            }
+        }
+    } 
+}
+
+class Huku extends Thread {
+    public void run() {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Huku");
             try {
                 Thread.sleep(1000);
             } catch (Exception e) {
@@ -14,6 +27,9 @@ class Hi {
 class Main {
     public static void main(String[] args) {
         Hi t1 = new Hi();
-        t1.run();
+        t1.start();
+
+        Huku t2 = new Huku();
+        t2.start();
     }
 }
