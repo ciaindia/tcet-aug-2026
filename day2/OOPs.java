9d4d54a4257288428d598f073879d2102fca1441
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-08-22 11:55:48 +0530

    day 2

--- day2/OOPs.java
+++ day2/OOPs.java
@@ -13,13 +13,30 @@ class Bank {
         this.balance = this.balance + amount;
     }
 
+    public void withdraw(int amount) {
+        this.balance = this.balance - amount;
+    }
+
+}
+
+class HDFC extends Bank {
+    public HDFC(int b) {
+        super(b);
+    }
 }
 
 class Main {
     public static void main(String[] args) {
-        Bank acc1 = new Bank(1000);
-        acc1.showBalance();
-        acc1.deposit(500);
-        acc1.showBalance();
+        // Bank acc1 = new Bank(1000);
+        // acc1.showBalance();
+        // acc1.deposit(500);
+        // acc1.showBalance();
+
+        HDFC acc2 = new HDFC(10000);
+        acc2.showBalance();
+        acc2.deposit(5000);
+        acc2.showBalance();
+        acc2.withdraw(2000);
+        acc2.showBalance();
     }
 }
