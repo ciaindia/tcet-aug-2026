88bb7e7e1935e17a927040526f37c8efd4eb7221
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-08-22 10:02:38 +0530

    day 2

---
+++ day2/Circle.java
@@ -0,0 +1,23 @@
+class Circle {
+    
+    int radius;
+
+    public Circle(int r) {
+        this.radius = r;
+    }
+
+    public void area() {
+        System.out.println("I am from Circle area with radius " + this.radius);
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Circle cobj1 = new Circle(5);
+        cobj1.area();
+
+        Circle cobj2 = new Circle(7);
+        cobj2.area();
+    }
+}
